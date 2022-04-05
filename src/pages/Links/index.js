ec47082a2339dcf7e5531427ed422a666a6178e6
Pedro Rocha <pedrorocha73658@gmail.com>
2022-04-05 09:54:46 -0300

    Update page links, components links make auto

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,40 +1,52 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 import './links.css'
-import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
-import {Link} from 'react-router-dom'
-import {getLinksSave} from '../../services/storeLinks'
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { getLinksSave } from '../../services/storeLinks'
 
 export default function Links() {
 
+  const [myLinks, setMyLinks] = useState([]);
+  const [data, setData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
-    async function getLinks(){
+    async function getLinks() {
       const result = await getLinksSave('@encurtaLink');
 
-      console.log(result)
-    }
+      if (result === 0) {
+        //A lista está vazia
+        console.log('A LISTA ESTÁ VAZIA!');
+      }
+      setMyLinks(result);
 
+    }
     getLinks();
+
   }, []);
 
 
-    return (
-      <div className='links-container'>
-        <div className='links-header'>
-          <Link to='/'>
-            <FiArrowLeft size={38} color='#fff'/>
-          </Link>
-          <h1>Meus Links</h1>
-        </div>
-        <div className='links-item'>
+  return (
+    <div className='links-container'>
+      <div className='links-header'>
+        <Link to='/'>
+          <FiArrowLeft size={38} color='#fff' />
+        </Link>
+        <h1>Meus Links</h1>
+      </div>
+
+      {myLinks.map(link => (
+        <div key={link.id} className='links-item'>
           <button className='link'>
-            <FiLink size={18} color='#fff'/>
-            http://sujeitoprogramador.com
+            <FiLink size={18} color='#fff' />
+            {link.long_url}
           </button>
           <button className='link-delete'>
-            <FiTrash size={18} color='#ff5454'/>
+            <FiTrash size={18} color='#ff5454' />
           </button>
         </div>
-      </div>
-    );
+      ))}
+    </div>
+  );
 }

14a88c4adb2c112a74b28187d61b8e9eb8bb95d8
Pedro Rocha <pedrorocha73658@gmail.com>
2022-04-04 22:30:22 -0300

    Update axios

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export const key = "3aafebafc255849c8d002125b382e256bcaecf73";
 const api = axios.create({
     baseURL: 'https://api-ssl.bitly.com/v4',
     headers:{
-        'Autorization': `Bearer ${key}`
+        'Authorization': `Bearer ${key}`
     }
 })
 

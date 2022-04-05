990dc3ebd384a049782997ff4952d22594a2a57a
Pedro Rocha <pedrorocha73658@gmail.com>
2022-04-05 09:35:20 -0300

    Add useEffect to return elements

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,8 +1,23 @@
+import {useState, useEffect} from 'react'
+
 import './links.css'
 import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
+import {getLinksSave} from '../../services/storeLinks'
 
 export default function Links() {
+
+  useEffect(() => {
+    async function getLinks(){
+      const result = await getLinksSave('@encurtaLink');
+
+      console.log(result)
+    }
+
+    getLinks();
+  }, []);
+
+
     return (
       <div className='links-container'>
         <div className='links-header'>
@@ -11,16 +26,6 @@ export default function Links() {
           </Link>
           <h1>Meus Links</h1>
         </div>
-        <div className='links-item'>
-          <button className='link'>
-            <FiLink size={18} color='#fff'/>
-            http://sujeitoprogramador.com
-          </button>
-          <button className='link-delete'>
-            <FiTrash size={18} color='#ff5454'/>
-          </button>
-        </div>
-
         <div className='links-item'>
           <button className='link'>
             <FiLink size={18} color='#fff'/>
